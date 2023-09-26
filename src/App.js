5acda01def25645219e2bbe216835bac3179f13e
mwebaze-nicho <nickmwebaze@gmail.com>
2023-09-26 12:01:46 +0300

    Added env varaible

--- src/App.js
+++ src/App.js
@@ -1,61 +1,58 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import './App.css';
-import SearchIcon from './search.svg'
-import MovieCard from "./MovieCard";   
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+// import dotenv from "dotenv";
+// dotenv.config();
 
+const App = () => {
+  const [movies, setMovies] = useState([]); //creates a state to gather results array
+  const [searchTerm, setSearchTerm] = useState("");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = "http://www.omdbapi.com?apikey=" + API_KEY;
+  // http://www.omdbapi.com?apikey="key"&s=$"action" this can give a json file in the browser
 
-const App =() => {
-    const [movies, setMovies] =useState([]); //creates a state to gather results array
-    const [searchTerm, setSearchTerm] = useState('');
-    const API_KEY = process.env.API_KEY;
-    const API_URL ='http://www.omdbapi.com?apikey=' + API_KEY;
-    // http://www.omdbapi.com?apikey="key"&s=$"action" this can give a json file in the browser
-  
+  const searchMovies = async (title1) => {
+    //this will search movies from the api and takes time
+    const response = await fetch(`${API_URL}&s=${title1}`); //call API for response
+    const data = await response.json(); //returns the fetched data in JSON format
+    setMovies(data.Search); //this will return all the results to the user
+  };
+  useEffect(() => {
+    searchMovies("Love");
+    console.log(API_KEY);
+  }, []);
+  return (
+    <div className="app">
+      <h1>{API_KEY} MovieZone</h1>
 
-    const searchMovies = async (title1)=> {  //this will search movies from the api and takes time
-        const response = await fetch (`${API_URL}&s=${title1}`); //call API for response
-        const  data = await response.json(); //returns the fetched data in JSON format
-        setMovies(data.Search); //this will return all the results to the user
+      <div className="search">
+        <input
+          placeholder="Search for your favourite movies"
+          value={searchTerm} //this allows us to searchbox to hold the name of the search
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-    }
-    useEffect (()=>{
-        searchMovies('Love');
-    }, []);
-    return (
-        <div className="app">
-           <h1>MovieZone</h1>
-
-           <div className="search">
-            <input placeholder="Search for your favourite movies"
-                value= {searchTerm}   //this allows us to searchbox to hold the name of the search
-                onChange={(e)=> setSearchTerm(e.target.value)}
-            />
-            <img
-                src={SearchIcon}
-                alt="search"
-                onClick={()=>searchMovies(searchTerm)}
-            />
-           </div>
-
-            {
-                movies?.length > 0
-                    ? (
-                        <div className="container">
-                            {movies.map((movie)=>(
-                                <MovieCard movie ={movie} />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                        <h2>No movies found</h2>
-                        </div>
-                    )
-            }
-
-           
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import dotenv from "dotenv";
+import App from "./App";
 
-import App from './App';
 
-const root =ReactDOM.createRoot(document.getElementById('root'));
+// dotenv.config();
+// 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <App/>
-);
+root.render(<App />);
