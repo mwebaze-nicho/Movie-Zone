be1648acbff28218d6df9ec7640488e6ad9fa725
mwebaze-nicho <nickmwebaze@gmail.com>
2023-10-04 19:27:40 +0300

    *

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import dotenv from "dotenv";
 import App from "./App";
 
-
-// dotenv.config();
-// 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
